24abc320aa22244432f84c5a213507042d76aace
GAURAV RAMI <gauravrami@gauravs-macbook-pro.local>
2018-04-01 21:08:27 +0530

    Initial Commit

---
+++ DemoAPICall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoAPICall
+//
+//  Created by GAURAV RAMI on 01/04/18.
+//  Copyright © 2018 GAURAV RAMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
